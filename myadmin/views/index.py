b3d982ca24db9881a5a310341b1c0ab79e7fd790
LAPTOP-NPIRO3I8\sunys <1083010558@qq.com>
2022-02-04 23:31:14 +0800

    模板引入，myadmin首页调试

--- myadmin/views/index.py
+++ myadmin/views/index.py
@@ -4,8 +4,9 @@
 #@File : index.py
 #@Software : PyCharm
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
     #接收到前端的处理逻辑，返回”欢迎来到点餐系统后台！“
-    return HttpResponse("欢迎来到点餐系统后台！")
+    return render(request,'myadmin/index/index.html')  #render渲染函数，返回渲染的页面
