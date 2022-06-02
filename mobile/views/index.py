58e4106d7a13fe102b1daffdc6a3e7df4a2b8365
LAPTOP-NPIRO3I8\sunys <1083010558@qq.com>
2022-06-02 14:27:53 +0800

    H5手机移动端点餐："主页调试-动态页面"和店铺切换

--- mobile/views/index.py
+++ mobile/views/index.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from myadmin.models import Category, Product
+from myadmin.models import Category, Product, Shop
 
 
 def index(request):
@@ -16,40 +16,30 @@ def index(request):
 
     '''移动端首页'''
     #获取并判断当前店铺信息
-    # shopinfo = request.session.get("shopinfo", None)
-    # if shopinfo is None:
-    #     return redirect(reverse("mobile_shop")) # 重定向到店铺选择页
-    # # 获取当前店铺下的菜品类别和菜品信息
-    # clist = Category.objects.filter(shop_id=shopinfo['id'],status=1)
-    # productlist = dict()
-    # for vo in clist:
-    #     plist = Product.objects.filter(category_id=vo.id, status=1)
-    #     productlist[vo.id] = plist
-    #     context = {'categorylist': clist, 'productlist': productlist.items(), 'cid': clist[0]}
-    return render(request, "mobile/index.html")
+    shopinfo = request.session.get("shopinfo", None)
+    if shopinfo is None:
+        return redirect(reverse("mobile_shop")) # 重定向到店铺选择页
+    # 获取当前店铺下的菜品类别和菜品信息
+    clist = Category.objects.filter(shop_id=shopinfo['id'],status=1)
+    productlist = dict()
+    for vo in clist:
+        plist = Product.objects.filter(category_id=vo.id, status=1)
+        productlist[vo.id] = plist
+        context = {'categorylist': clist, 'productlist': productlist.items(), 'cid': clist[0]}
+    return render(request, "mobile/index.html", context)
 
 
+def shop(request):
+    '''呈现店铺选择页面'''
+    context = {'shoplist': Shop.objects.filter(status=1)}
+    return render(request, 'mobile/shop.html', context)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def selectShop(request):
+    '''执行店铺选择'''
+    # 获取店铺id号，通过店铺id号获取店铺信息
+    sid = request.GET['sid']
+    ob = Shop.objects.get(id=sid)
+    # 将店铺信息放入到seession中
+    request.session['shopinfo'] = ob.toDict()
+    return redirect(reverse('mobile_index'))
